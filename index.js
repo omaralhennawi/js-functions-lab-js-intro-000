98a8e66545c779dd0d1331a348bec7c25c7a6235
Omar Al Hennawi <omar504969@gmail.com>
2021-01-18 17:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 function happyHolidays(){
   var string = "Happy holidays!"
-  return string
+  return string;
 }
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
